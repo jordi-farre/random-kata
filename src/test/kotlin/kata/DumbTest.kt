cb590e6ad8821177972e68868151d803a3e48fa0
Jordi Farré <j.farre@lookiero.com>
2021-03-25 15:11:18 +0100

    create repository

---
+++ src/test/kotlin/kata/DumbTest.kt
@@ -0,0 +1,13 @@
+package kata
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DumbTest {
+
+    @Test
+    fun `just a dumb test`() {
+        assertThat(true).isTrue
+    }
+
+}
